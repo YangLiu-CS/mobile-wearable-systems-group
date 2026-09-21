b80c65814b9150c2f177cdfce8a0e9b16899cfd8
Hana <hana@Hanas-MacBook-Pro.local>
2026-09-21 12:05:49 -0400

    Deploy undergraduate student update

--- _next/static/chunks/app/people/page-5178b1dfbc20dcd1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[284],{3301:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3466,23))}},e=>{e.O(0,[466,221,230,358],()=>e(e.s=3301)),_N_E=e.O()}]);
